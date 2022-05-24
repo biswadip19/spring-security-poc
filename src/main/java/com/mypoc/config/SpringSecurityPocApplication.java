629dec3a4d12bd5693d59c44d946b10195e1efcd
bmukherjee <19.biswadip@gmail.com>
2022-05-24 17:53:16 +0530

    Spring boot Actuator related changes added

--- src/main/java/com/mypoc/config/SpringSecurityPocApplication.java
+++ src/main/java/com/mypoc/config/SpringSecurityPocApplication.java
@@ -1,10 +1,12 @@
 package com.mypoc.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 @ComponentScan("com.mypoc")
 public class SpringSecurityPocApplication {
 
